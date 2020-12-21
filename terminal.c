5c98c2947e0fd094ef922c73d859465023782534
Shaneal Findley <sfindley899@gmail.com>
2020-12-21 14:30:40 -0500

    add timeout

--- terminal.c
+++ terminal.c
@@ -3,6 +3,7 @@
 #include <termios.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 struct termios original_termios;
 
@@ -41,19 +42,24 @@ void enableRawMode(void)
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() 
 {	
-	char c;
-
 	enableRawMode();
-
-	// loop to read 1 from standard input
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	char c;
+ 
+	// infinite loop to read 1 from standard input
+	while (true)
 	{
+		c = '/0';
+
+		read(STDIN_FILENO, &c, 1);
+
 		// echos control character value otherwise ASCII and character value
 		if(iscntrl(c))
 		{
@@ -63,6 +69,7 @@ int main()
 		{
 			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 
 	return 0;
