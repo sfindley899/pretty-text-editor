f45961b24f90c989a849690252b449c86722a339
Shaneal Findley <sfindley899@gmail.com>
2020-12-21 00:05:28 -0500

    restore original terminal settings

--- terminal.c
+++ terminal.c
@@ -1,8 +1,53 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+
+struct termios original_termios;
+
+
+/** 
+ * 	disableRawMode
+ * 
+ * 	@param none
+ *  
+ *	Restores original terminal attributes
+ */
+void disableRawMode(void)
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+/** 
+ * 	enableRawMode
+ * 
+ * 	@param none
+ *  
+ *	Sets terminal attributes to disable
+ *	echo of keystrokes in terminal
+ */
+void enableRawMode(void)
+{
+	// Saves default terminal settings
+	tcgetattr(STDIN_FILENO, &original_termios);
+
+	// Call disableRawMode
+	atexit(disableRawMode);
+
+	struct termios raw = original_termios;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() 
-{
+{	
 	char c;
+
+	enableRawMode();
+
 	// loop to read 1 byte from standard input
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
