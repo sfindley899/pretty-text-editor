d0d21537e43424b648bb868b2728d9a2a0705ad1
Shaneal Findley <sfindley899@gmail.com>
2020-12-28 17:31:11 -0500

    search match highlight

--- terminal.c
+++ terminal.c
@@ -41,7 +41,8 @@ enum editorKey
 
 enum editorHighlight {
 	HL_NORMAL = 0,
-		HL_NUMBER
+		HL_NUMBER,
+		HL_MATCH
 };
 
 /*** structs ***/
@@ -656,6 +657,7 @@ void editorFindCallback(char * query, int key)
 			editor.cy = current;
 			editor.cx = editorRowRxToCx(row, match - row->render);
 			editor.rowoff = editor.numrows;
+			memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
 			break;
 		}
 	}
@@ -1527,7 +1529,10 @@ int editorSyntaxToColor(int hl)
 			{
 				return 31;
 			}
-		
+		case HL_MATCH:
+			{
+				return 34;
+			}	
 		default:
 			{
 				return 37;
